dcfa0ec23881ae7cd3f8262ed7d90c3b5cfc035b
Falgun <77203052+fxlgun@users.noreply.github.com>
2023-06-08 19:50:13 +0530

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://e-commerce-api-drab.vercel.app/";
+const BASE_URL = "https://e-commerce-api-drab.vercel.app/api";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
